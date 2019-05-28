e4aea378489c858c52b7b857402f1ea3344a186a
Christopher Ponce Mendez <christopherponcedev@gmail.com>
2019-05-28 15:08:28 -0400

    Initial commit

---
+++ click count/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  click count
+//
+//  Created by Christopher Ponce Mendez on 5/28/19.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
