d2725948ce5f3f8e64ad2d1adf7f5de0a2dd8766
Christopher Ponce Mendez <christopherponcedev@gmail.com>
2019-05-28 15:32:49 -0400

    added incrementCount function 2 ui elements button and label

--- click count/ViewController.swift
+++ click count/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //label
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 50)
+        label.text = "0"
+        self.label = label
+        view.addSubview(label)
+        
+        //Button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
     }
 
-
+    @objc func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
 }
 
